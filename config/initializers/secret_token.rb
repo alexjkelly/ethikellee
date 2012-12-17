e47e29f0e89886421a73aae282d8c0996540d8af
Alex Kelly <alex.kelly@william-lee.com>
2012-12-17 17:38:09 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ethikellee::Application.config.secret_token = '4f6e4c95b2e0bf1bb7d6e2714b74f538ccb387f660bed0a89386ee7e5fa0a021464a8ac87607ac73b7a31e44360496b07568de46d5e58ae610dfe1e5a175e0dd'
