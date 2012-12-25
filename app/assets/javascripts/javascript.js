d37cd19f36a55e73042d7e7e2f329385afd81438
William Lee <me@william-lee.com>
2012-12-25 18:25:58 +0000

    updates home page

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
   $('body').hide().fadeIn('slow');
   
+  $('#btnShowcase').click(function(){
+    $('#about').hide();
+    $('#showcase').fadeIn('slow');
+  });
+  
+  $('#btnAbout').click(function(){
+    $('#showcase').fadeOut();
+    $('#about').fadeIn('slow');
+  });
 });
